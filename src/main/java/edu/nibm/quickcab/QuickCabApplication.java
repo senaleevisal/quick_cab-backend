9b70a2efa47cd5770069b35d889fb675afdf00c9
Senalee Visal <m.m.senaleevisal@gmail.com>
2024-09-10 08:53:55 +0530

    initial commit

---
+++ src/main/java/edu/nibm/quickcab/QuickCabApplication.java
@@ -0,0 +1,13 @@
+package edu.nibm.quickcab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickCabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickCabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/nibm/quickcab/QuickCabApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nibm.quickcab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickCabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
